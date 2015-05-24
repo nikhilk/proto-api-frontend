69b94843e4e28f3fbcfdcd695ed5b57ffc145bdf
Nikhil Kothari <nikhilko@google.com>
2015-05-23 17:03:58 -0700

    Use static middleware

--- api/main.js
+++ api/main.js
@@ -5,16 +5,6 @@ var express = require('express'),
 
 var API_PORT = 3976;
 
-function pageHandler(request, response) {
-  var html = fs.readFileSync(__dirname + '/page.html', { encoding: 'utf8' });
-
-  response.set({
-    'Content-Type': 'text/html'
-  });
-  response.send(html);
-  response.end();
-}
-
 function dataHandler(request, response) {
   response.json(request.body);
   response.end();
@@ -23,8 +13,8 @@ function dataHandler(request, response) {
 function createApplication() {
   var app = express();
   app.use(bodyParser.json());
+  app.use('/content', express.static('.'));
 
-  app.get('/content/page.html', pageHandler);
   app.post('/api/data', dataHandler);
 
   return app;
